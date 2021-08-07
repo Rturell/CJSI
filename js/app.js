c1831f4e25d72a44b1de378008c84db1b7cf3146
Rturell <rodrigoturell@gmail.com>
2021-08-06 22:25:48 -0300

    first commit

---
+++ js/app.js
@@ -0,0 +1,34 @@
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 3,
+    spaceBetween: 20,
+    freeMode: true,
+    speed: 200,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    breakpoints: {
+        // when window width is >= 320px
+        320: {
+            slidesPerView: 1,
+            spaceBetween: 20,
+        },
+        // when window width is >= 480px
+        480: {
+            slidesPerView: 2,
+            spaceBetween: 30
+        },
+        640: {
+            slidesPerView: 2,
+            spaceBetween: 40
+        },
+        768: {
+            slidesPerView: 3,
+            spaceBetween: 40
+        },
+         992: {
+             slidesPerView: 4,
+             spaceBetween: 10
+         }
+    }
+});
